d84e7e5d39881d873aa742e36c72ad6a35e4ef75
webdev-ashishk <webdev.ashishk@gmail.com>
2023-12-05 21:10:16 +0530

    this is basic introduction of nextjs

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import { Tweet } from "react-tweet";
+export default function HomePage() {
+  return (
+    <div>
+      <h1>this is home page @</h1>
+      <Tweet id="1629307668568633344" />
+    </div>
+  );
+}
